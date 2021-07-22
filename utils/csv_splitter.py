e4bf11716f4c7e4fe4cf1ba5d5fb7420fa5abc38
juyingnan <bunny_gg@hotmail.com>
2021-07-22 01:19:45 -0400

    minor changes

--- utils/csv_splitter.py
+++ utils/csv_splitter.py
@@ -1,11 +1,11 @@
-def tryParseInt(s, base=10, val=None):
+def try_parse_int(s, base=10, val=None):
     try:
         return int(s, base)
     except ValueError:
         return val
 
 
-target_root_path = "G:\GE\skin_12_data"
+target_root_path = r"G:\GE\skin_12_data"
 cell_file_path = r"C:\Users\bunny\Desktop\new\Centroids_New.txt"
 
 # bv_file = open(bv_file_path, 'r')
@@ -25,42 +25,42 @@ headline = "ID,X,Y,Z,cell_type\n"
 
 current_region = ""
 current_type = ""
-for line in cell_lines:
-    l = line.strip()
-    if l.startswith("Region"):
-        cells[l] = {}
-        current_region = l
+for line_content in cell_lines:
+    line = line_content.strip()
+    if line.startswith("Region"):
+        cells[line] = {}
+        current_region = line
         current_type = ""
         continue
-    elif l.startswith("Mach") or \
-            l.startswith("Blood") or \
-            l.startswith("T"):
+    elif line.startswith("Mach") or \
+            line.startswith("Blood") or \
+            line.startswith("T"):
         assert current_region != ""
-        current_type = l
+        current_type = line
         cells[current_region][current_type] = []
-    elif l.startswith("Label"):
+    elif line.startswith("Label"):
         continue
     else:
         assert current_region != ""
         assert current_type != ""
         if current_type in type_abr:
-            cells[current_region][current_type].append(l + "," + type_abr[current_type])
+            cells[current_region][current_type].append(line + "," + type_abr[current_type])
         else:
             print(f"{current_region}, {current_type} not in the type dict")
 
 # write csv
 for region in cells:
     print(region, ":")
-    for type in cells[region]:
-        print(f"\t{type}: {len(cells[region][type])}")
-    region_id = tryParseInt(region[6:])
+    for cell_type in cells[region]:
+        print(f"\t{cell_type}: {len(cells[region][cell_type])}")
+    region_id = try_parse_int(region[6:])
     if region_id is None:
         print(f"Region name {region} fails to parse")
         break
     target_file_path = target_root_path + rf"\region_{region_id}\centroids.csv"
     csv_file = open(target_file_path, "w")
     csv_file.write(headline)
-    for type in cells[region]:
-        for line in cells[region][type]:
+    for cell_type in cells[region]:
+        for line in cells[region][cell_type]:
             csv_file.write(line + "\n")
     csv_file.close()

cdc16af80cc860a458fa1ae65aa8f362946eb28b
juyingnan <bunny_gg@hotmail.com>
2021-08-16 00:21:15 -0400

    better format

--- utils/mean_dist_calculator.py
+++ utils/mean_dist_calculator.py
@@ -3,10 +3,10 @@ import statistics
 target_root_path = r"G:\GE\skin_12_data"
 
 for i in range(4):
-    print("\tAverage\tMedian", end='')
+    print("\t\tAverage\tMedian\t%", end='')
 print()
 
-for region_id in range(1, 13):
+for region_id in [11, 3, 6, 8, 9, 1, 12, 5, 4, 2, 10, 7]:
     target_file_path = target_root_path + rf"\region_{region_id}\nuclei.csv"
 
     cell_file = open(target_file_path, 'r')
@@ -36,4 +36,83 @@ for region_id in range(1, 13):
         print("\t", key, end='')
         print(f"\t{statistics.mean(distances[key])}", end='')
         print(f"\t{statistics.median(distances[key])}", end='')
+        print(f"\t{len(distances[key]) / len(distances['all'])}", end='')
     print()
+#
+# distances = {'all': []}
+# for region_id in [1, 3, 6, 8, 9, 11]:
+#     target_file_path = target_root_path + rf"\region_{region_id}\nuclei.csv"
+#
+#     cell_file = open(target_file_path, 'r')
+#     cell_file.readline()
+#     cell_lines = cell_file.readlines()
+#
+#     for line in cell_lines:
+#         content = line.split(',')
+#         type = content[4]
+#         distance = content[5]
+#         if type not in distances:
+#             distances[type] = []
+#         else:
+#             distances[type].append(float(distance))
+#             distances['all'].append(float(distance))
+#
+# print("Sun", end='')
+# for key in ['all', 'CD68', 'T-Reg', 'T-Helper']:
+#     print("\t", key, end='')
+#     print(f"\t{statistics.mean(distances[key])}", end='')
+#     print(f"\t{statistics.median(distances[key])}", end='')
+#     print(f"\t{len(distances[key]) / len(distances['all'])}", end='')
+# print()
+#
+# distances = {'all': []}
+# for region_id in [2, 4, 5, 7, 10, 12]:
+#     target_file_path = target_root_path + rf"\region_{region_id}\nuclei.csv"
+#
+#     cell_file = open(target_file_path, 'r')
+#     cell_file.readline()
+#     cell_lines = cell_file.readlines()
+#
+#     for line in cell_lines:
+#         content = line.split(',')
+#         type = content[4]
+#         distance = content[5]
+#         if type not in distances:
+#             distances[type] = []
+#         else:
+#             distances[type].append(float(distance))
+#             distances['all'].append(float(distance))
+#
+# print("Non-sun", end='')
+# for key in ['all', 'CD68', 'T-Reg', 'T-Helper']:
+#     print("\t", key, end='')
+#     print(f"\t{statistics.mean(distances[key])}", end='')
+#     print(f"\t{statistics.median(distances[key])}", end='')
+#     print(f"\t{len(distances[key]) / len(distances['all'])}", end='')
+# print()
+#
+# distances = {'all': []}
+# for region_id in range(1, 13):
+#     target_file_path = target_root_path + rf"\region_{region_id}\nuclei.csv"
+#
+#     cell_file = open(target_file_path, 'r')
+#     cell_file.readline()
+#     cell_lines = cell_file.readlines()
+#
+#     for line in cell_lines:
+#         content = line.split(',')
+#         type = content[4]
+#         distance = content[5]
+#         if type not in distances:
+#             distances[type] = []
+#         else:
+#             distances[type].append(float(distance))
+#             distances['all'].append(float(distance))
+#
+# print("All ", end='')
+# for key in ['all', 'CD68', 'T-Reg', 'T-Helper']:
+#     print("\t", key, end='')
+#     print(f"\t{statistics.mean(distances[key])}", end='')
+#     print(f"\t{statistics.median(distances[key])}", end='')
+#     print(f"\t{len(distances[key]) / len(distances['all'])}", end='')
+# print()

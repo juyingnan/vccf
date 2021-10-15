6837cfa4a0b8aebd0c7233d82f9a5446b3ddf6a9
juyingnan <bunny_gg@hotmail.com>
2021-10-15 16:47:04 -0400

    adapted for cover design

--- visualization/distance_bokeh.py
+++ visualization/distance_bokeh.py
@@ -61,8 +61,9 @@ if __name__ == '__main__':
         ("positive", "#aeaeb8"),
     ])
 
+    index = 1
     p = figure(match_aspect=True,
-               plot_width=int(3727 * 1.075), plot_height=int(2348 * 1.075),
+               plot_width=int(3727 * index), plot_height=int(2348 * index),
                tools=tools_list, output_backend="svg",
                # title='nuclei/vessel distance',
                )
@@ -76,12 +77,14 @@ if __name__ == '__main__':
     p.background_fill_alpha = 0.0
     p.outline_line_color = None
 
-    p.image_url(url=[
-        rf'G:\GE\HubmapDemoDay_Dec14th_2020_Vessel_Nuclei_Segmentation_NucleiQuantification\output\dapi_Slide{str(input_id)}_region6_nucseg.png'],
-        x=0, y=0, anchor="bottom_left")
+    # p.image_url(url=[
+    #     rf'G:\GE\HubmapDemoDay_Dec14th_2020_Vessel_Nuclei_Segmentation_NucleiQuantification\output\dapi_Slide{str(input_id)}_region6_nucseg.png'],
+    #     x=0, y=0, anchor="bottom_left")
     v_headers, v_columns = read_csv(vessel_file_path)
     for i in range(0, len(v_headers)):
         v_columns[v_headers[i]] = [float(value) for value in v_columns[v_headers[i]]]
+    v_columns['y'] = [float(value) for value in v_columns['y']]
+    max_y = max(v_columns['y'])
     v_columns['y'] = [float(value) for value in v_columns['y']]
 
     data = dict()
@@ -89,7 +92,7 @@ if __name__ == '__main__':
         data[header] = v_columns[header]
     v_df = pd.DataFrame(data)
 
-    p.circle(x='x', y='y', source=v_df, color='#91672c', size=4, alpha=1)
+    p.circle(x='x', y='y', source=v_df, color='#00e0e0', size=1, alpha=0.7)
 
     n_headers, n_columns = read_csv(nuclei_file_path)
     for i in range(1, len(n_headers)):
@@ -97,8 +100,9 @@ if __name__ == '__main__':
     n_columns['y'] = [float(value) for value in n_columns['y']]
     n_columns['vy'] = [float(value) for value in n_columns['vy']]
     n_columns['alpha'] = [(float(value) + 200) / 800 for value in n_columns['distance']]
-    n_columns['color'] = ['#%02x%02x%02x' % (0, int(255 * value * 0.3 / 160), int(255 * (160 - value * 0.3) / 160))
-                          for value in n_columns['distance']]
+    # n_columns['color'] = ['#%02x%02x%02x' % (0, int(255 * value * 0.3 / 160), int(255 * (160 - value * 0.3) / 160))
+    #                       for value in n_columns['distance']]
+    n_columns['color'] = ['#828282' for value in n_columns['distance']]
 
     data = dict()
     for header in n_headers:
@@ -107,8 +111,8 @@ if __name__ == '__main__':
         data['color'] = n_columns['color']
     n_df = pd.DataFrame(data)
 
-    p.segment(x0='x', y0='y', x1='vx', source=n_df, y1='vy', color="red", alpha=0.2, line_width=1)
-    circle = p.circle(x='x', y='y', source=n_df, color='color', alpha=0.7, size=5)
+    p.segment(x0='x', y0='y', x1='vx', source=n_df, y1='vy', color="ivory", alpha=0.4, line_width=1)
+    circle = p.circle(x='x', y='y', source=n_df, color='color', alpha=0.5, size=2.5)
     g1_hover = HoverTool(renderers=[circle], tooltips=[('X', "@x"), ('Y', "@y"), ('distance', "@distance")])
     p.add_tools(g1_hover)
 

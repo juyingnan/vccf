ddcf7dd56e35b866ea0c1e294ce99a1617b2eec4
juyingnan <bunny_gg@hotmail.com>
2021-12-06 18:19:10 -0500

    added tool to convert from tiff to csv for bokeh

--- visualization/kidney_distance_bokeh_cover.py
+++ visualization/kidney_distance_bokeh_cover.py
@@ -43,7 +43,7 @@ if __name__ == '__main__':
     if len(sys.argv) >= 2:
         input_id = sys.argv[1]
     output_root_path = rf'C:\Users\bunny\Desktop\Region3_Slide88_Visualization'
-    nuclei_output_name = 'seg.csv'
+    nuclei_output_name = 'nuclei.csv'
     nuclei_file_path = os.path.join(output_root_path, nuclei_output_name)
     vessel_output_name = 'vessel.csv'
     vessel_file_path = os.path.join(output_root_path, vessel_output_name)
@@ -97,9 +97,9 @@ if __name__ == '__main__':
 
     n_headers, n_columns = read_csv(nuclei_file_path)
     n_columns['y'] = [float(value) for value in n_columns['y']]
-    # n_columns['vy'] = [float(value) for value in n_columns['vy']]
+    n_columns['vy'] = [float(value) for value in n_columns['vy']]
     n_columns['x'] = [float(value) for value in n_columns['x']]
-    # n_columns['vx'] = [float(value) for value in n_columns['vx']]
+    n_columns['vx'] = [float(value) for value in n_columns['vx']]
     n_columns['alpha'] = [0.5] * len(n_columns['y'])
     n_columns['color'] = [color_dict[type] for type in n_columns['type']]
     n_columns['nuclei'] = [False if type in ['P53', 'KI67', 'DDB2'] else True for type in n_columns['type']]
@@ -107,6 +107,9 @@ if __name__ == '__main__':
     n_columns['id'].append(len(n_columns['y']))
     n_columns['y'].append(0)
     n_columns['x'].append(0)
+    n_columns['vy'].append(0)
+    n_columns['vx'].append(0)
+    n_columns['distance'].append(0)
     n_columns['type'].append("placeholder")
     n_columns['alpha'].append(0.5)
     n_columns['color'].append(color_dict["placeholder"])
@@ -115,6 +118,9 @@ if __name__ == '__main__':
     n_columns['id'].append(len(n_columns['y']))
     n_columns['y'].append(16758)
     n_columns['x'].append(9409)
+    n_columns['vy'].append(16758)
+    n_columns['vx'].append(9409)
+    n_columns['distance'].append(0)
     n_columns['type'].append("placeholder")
     n_columns['alpha'].append(0.5)
     n_columns['color'].append(color_dict["placeholder"])
@@ -151,9 +157,26 @@ if __name__ == '__main__':
         data['nuclei'] = n_columns['nuclei']
     n_df = pd.DataFrame(data)
 
+    v_headers, v_columns = read_csv(vessel_file_path)
+    for i in range(0, len(v_headers)):
+        v_columns[v_headers[i]] = [float(value) for value in v_columns[v_headers[i]]]
+
+    data = dict()
+    for header in v_headers:
+        data[header] = v_columns[header]
+    v_df = pd.DataFrame(data)
+
     # p.segment(x0='x', y0='y', x1='vx', source=n_df, y1='vy', color="ivory", alpha=0.4, line_width=1)
     circle = p.circle(x='y', y='x', source=n_df[n_df['nuclei'] == True], color='color', alpha=0.5, size=10)
     cross = p.cross(x='y', y='x', source=n_df[n_df['nuclei'] == False], color='color', alpha=0.5, size=10)
+    p.segment(x0='y', y0='x', x1='vy', y1='vx', source=n_df[n_df['nuclei'] == True],
+              color="red",
+              alpha=0.2, line_width=1,
+              )
+    p.scatter(x='vy', y='vx', source=v_df, fill_alpha=0.5, size=1,
+              # marker=factor_mark('style_label', markers, roman_label),
+              marker='circle',
+              color='red', )
     g1_hover = HoverTool(renderers=[circle], tooltips=[('X', "@x"), ('Y', "@y"), ('distance', "@distance")])
     p.add_tools(g1_hover)
 

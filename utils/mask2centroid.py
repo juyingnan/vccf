ed01e778808c7d653f7c65742ab751355b81d3cb
juyingnan <bunny_gg@hotmail.com>
2021-12-06 18:19:33 -0500

    added tool to convert from tiff to csv for bokeh

--- utils/mask2centroid.py
+++ utils/mask2centroid.py
@@ -1,3 +1,4 @@
+import math
 import os
 
 import imageio
@@ -13,6 +14,8 @@ import utils.kidney_nuclei_vessel_calculate as my_csv
 
 Image.MAX_IMAGE_PIXELS = None
 
+non_nuclei_types = ['P53', 'KI67', 'DDB2']
+
 # read mask image
 root_path = r"C:\Users\bunny\Desktop\Region3_Slide88_Visualization"
 image_list = ['CD4_S88_AFRemoved_pyr16_region_003_Prob_DAPI_Seg_BinThresh_WSI_Masked_CD3_Masked.tif',
@@ -21,15 +24,33 @@ image_list = ['CD4_S88_AFRemoved_pyr16_region_003_Prob_DAPI_Seg_BinThresh_WSI_Ma
               'DDB2_S88_AFRemoved_pyr16_region_003_Prob_DAPI_Seg_BinThresh_WSI_Masked.tif',
               'FOXP3_S88_AFRemoved_pyr16_region_003_Prob_DAPI_Seg_BinThresh_WSI_Masked_CD3_Masked.tif',
               'KI67_S88_AFRemoved_pyr16_region_003_Prob_DAPI_Seg_BinThresh_WSI_Masked.tif',
-              'P53_S88_AFRemoved_pyr16_region_003_Prob_DAPI_Seg_BinThresh_WSI_Masked.tif', ]
+              'P53_S88_AFRemoved_pyr16_region_003_Prob_DAPI_Seg_BinThresh_WSI_Masked.tif',
+              ]
 resize_index = 1
 if len(sys.argv) >= 2:
     image_path = sys.argv[1]
 if len(sys.argv) >= 3:
     resize_index = int(sys.argv[2])
 preview = False
-cells = {}
 
+vessels = []
+blood_vessel_image_path = 'CD31_S88_AFRemoved_pyr16_region_003_Prob_WSI_Masked_BinThresh0.tif'
+blood_vessel_img = imageio.imread(os.path.join(root_path, blood_vessel_image_path))
+cell_type = blood_vessel_image_path.split('_')[0]
+if len(blood_vessel_img.shape) == 2:
+    mask = blood_vessel_img
+else:
+    mask = np.array(blood_vessel_img[:, :, 0])
+vessel_location = np.where(mask > 0.5)
+vessel_index = 10
+count = 0
+for x, y in zip(vessel_location[0], vessel_location[1]):
+    count += 1
+    if count % vessel_index == 0:
+        vessels.append([x, y])
+print(f"{len(vessels)} vessels")
+
+cells = {}
 for img_item in image_list:
     img_path = os.path.join(root_path, img_item)
     img = imageio.imread(img_path)
@@ -58,7 +79,20 @@ for img_item in image_list:
     for polygon in polygons:
         x = sum([polygon[i][0] for i in range(len(polygon))]) // len(polygon)
         y = sum([polygon[i][1] for i in range(len(polygon))]) // len(polygon)
-        cells[cell_type].append([x, y])
+        vx, vy = x, y
+        distance = 0
+        if cell_type not in non_nuclei_types:
+            distance = 500
+            for vessel_coor in vessels:
+                _vx, _vy = vessel_coor[0], vessel_coor[1]
+                if abs(_vx - x) <= distance and abs(_vy - y) <= distance:
+                    _dis = math.sqrt((_vx - x) ** 2 + (_vy - y) ** 2)
+                    if _dis < distance:
+                        distance = _dis
+                        vx, vy = _vx, _vy
+            if distance == 300:
+                print("NO NEAR")
+        cells[cell_type].append([x, y, vx, vy, distance])
 
 print(cells)
 
@@ -67,6 +101,9 @@ id = 0
 id_list = []
 x_list = []
 y_list = []
+vx_list = []
+vy_list = []
+dis_list = []
 type_list = []
 
 for key in cells:
@@ -75,11 +112,36 @@ for key in cells:
         id += 1
         x_list.append(coor[0])
         y_list.append(coor[1])
+        vx_list.append(coor[2])
+        vy_list.append(coor[3])
+        dis_list.append(coor[4])
         type_list.append(key)
 
-my_csv.write_csv(os.path.join(root_path, "seg.csv"),
+my_csv.write_csv(os.path.join(root_path, "nuclei.csv"),
                  [id_list,
                   x_list,
                   y_list,
-                  type_list, ],
-                 ['id', 'x', 'y', 'type', ])
+                  type_list,
+                  vx_list,
+                  vy_list,
+                  dis_list],
+                 ['id', 'x', 'y', 'type', 'vx', 'vy', 'distance'])
+
+print(f"{len(id_list)} cells/damages saved")
+
+id = 0
+vid_list = []
+vx_list = []
+vy_list = []
+for vessel in vessels:
+    vid_list.append(id)
+    id += 1
+    vx_list.append(vessel[0])
+    vy_list.append(vessel[1])
+
+my_csv.write_csv(os.path.join(root_path, "vessel.csv"),
+                 [vid_list,
+                  vx_list,
+                  vy_list, ],
+                 ['vid', 'vx', 'vy'])
+print(f"{len(vid_list)} vessels saved")

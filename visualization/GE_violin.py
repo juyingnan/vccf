6c954b5e9189ac010af7d4416c9e6b4de051dcea
juyingnan <bunny_gg@hotmail.com>
2022-01-26 20:39:09 -0500

    updated violin chart format

--- visualization/GE_violin.py
+++ visualization/GE_violin.py
@@ -2,116 +2,31 @@ import os
 import plotly.graph_objects as go
 import pandas as pd
 from plotly.subplots import make_subplots
+from cell_defination import *
 
 target_root_path = r"G:\GE\skin_12_data"
 
 file_paths = []
 
-regions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+regions = [1, 2, 3, 4, 5,
+           # 6,
+           7, 8, 9, 10, 11, 12]
 # ages = ['72', '53', '38', '48', '33', '42', '69', '57', '60', '53_', '22', '32']
-ages = [72, 53, 38, 48, 33, 42, 69, 57, 60, 53.5, 22, 32]  # the second 53 -> 54
-genders = ['Male', 'Male', 'Male', 'Male', 'Female', 'Female', 'Male', 'Male', 'Male', 'Female', 'Female', 'Female', ]
-suns = ['Sun-Exposed', 'Non-Sun-Exposed', 'Sun-Exposed', 'Non-Sun-Exposed', 'Non-Sun-Exposed', 'Sun-Exposed',
+ages = [72, 53, 38, 48, 33,
+        # 42,
+        69, 57, 60, 53.5, 22, 32]  # the second 53 -> 54
+genders = ['Male', 'Male', 'Male', 'Male', 'Female',
+           # 'Female',
+           'Male', 'Male', 'Male', 'Female', 'Female', 'Female', ]
+suns = ['Sun-Exposed', 'Non-Sun-Exposed', 'Sun-Exposed', 'Non-Sun-Exposed', 'Non-Sun-Exposed',
+        # 'Sun-Exposed',
         'Non-Sun-Exposed', 'Sun-Exposed', 'Sun-Exposed', 'Non-Sun-Exposed', 'Sun-Exposed', 'Non-Sun-Exposed']
-olds = ['old', 'old', 'Young', 'Young', 'Young', 'Young', 'old', 'old', 'old', 'old', 'Young', 'Young']
+olds = ['old', 'old', 'Young', 'Young', 'Young',
+        # 'Young',
+        'old', 'old', 'old', 'old', 'Young', 'Young']
 
 # color_dict = {'Sun-Exposed': 'orange',
 #               'Non-Sun-Exposed': 'blue'}
-
-percentage_dict = {
-    'CD68': [0.184952978,
-             0.188191882,
-             0.126099707,
-             0.317140238,
-             0.078381795,
-             0.073265962,
-             0.588394062,
-             0.124542125,
-             0.131067961,
-             0.159292035,
-             0.072016461,
-             0.335238095,
-             ],
-    'T-Helper': [0.031347962,
-                 0.136531365,
-                 0.092375367,
-                 0.117323556,
-                 0.10619469,
-                 0.816628388,
-                 0.102564103,
-                 0.036630037,
-                 0.065533981,
-                 0.09439528,
-                 0.072016461,
-                 0.08,
-                 ],
-    'T-Reg': [0.78369906,
-              0.675276753,
-              0.781524927,
-              0.565536205,
-              0.815423515,
-              0.11010565,
-              0.309041835,
-              0.838827839,
-              0.803398058,
-              0.746312684,
-              0.855967078,
-              0.584761905,
-              ]
-
-}
-
-median_dict = {
-    'All': [29.87172576,
-            28.48157299,
-            26.51490147,
-            26.12967661,
-            24.8531857,
-            21.59722204,
-            28.48157299,
-            36.29053324,
-            28.12329995,
-            28.06307847,
-            25.07349198,
-            23.97081559, ],
-    'CD68': [25.18261275,
-             21.93882403,
-             24.11140809,
-             24.97169753,
-             35.26811591,
-             25.47469332,
-             35.1951737,
-             60.47445742,
-             22.59458342,
-             25.89953839,
-             78.27513044,
-             21.78417619, ],
-    'T-Helper': [26.12967661,
-                 33.11537765,
-                 25.59026882,
-                 24.61089343,
-                 46.8,
-                 21.44014925,
-                 33.29624603,
-                 40.94484094,
-                 24.39016195,
-                 24.59705835,
-                 31.7370446,
-                 29.59924473, ],
-    'T-Reg': [20.47242047,
-              24.52835094,
-              24.77112263,
-              26.10379283,
-              28.002857,
-              22.21440974,
-              38.21203999,
-              25.93418173,
-              26.12967661,
-              21.59722204,
-              26.01798108,
-              23.67215573, ]
-
-}
 
 color_dict = {'Sun-Exposed': 'black',
               'Non-Sun-Exposed': 'grey',
@@ -130,12 +45,12 @@ color_dict = {'Sun-Exposed': 'black',
 opacity_dict = {'Sun-Exposed': 0.7,
                 'Non-Sun-Exposed': 0.7}
 
-for region_id in range(1, 13):
+for region_id in regions:
     target_file_path = target_root_path + rf"\region_{region_id}\nuclei.csv"
     file_paths.append(target_file_path)
 
 data_list = []
-for index in range(12):
+for index in range(12 - 1):
     file_path = file_paths[index]
     df = pd.read_csv(file_path, index_col=None, header=0)
     for l, name in zip([regions, ages, genders, suns, olds], ["Region", "Age", "Gender", "Skin Type", "Age Group"]):
@@ -146,21 +61,29 @@ n_data = pd.concat(data_list, axis=0, ignore_index=True)
 
 print(n_data)
 
+cell_type_list = ['', 'CD68', 'T-Helper', 'T-Killer', 'T-Reg']
+cell_dict[''] = {}
+cell_dict['']['legend'] = 'All Cells'
+
 # display box and scatter plot along with violin plot
 # fig = pt.violin(n_data, y="distance", x="Age", color="Skin Type",
 #                 box=True, hover_data=n_data.columns,
 #                 points='all',
 #                 )
+subplot_titles = []
+for cell_type in cell_type_list:
+    subplot_titles.append(cell_dict[cell_type]['legend'])
 fig = make_subplots(
-    rows=4, cols=1,
-    row_heights=[0.25, 0.25, 0.25, 0.25],
+    rows=5, cols=1,
+    row_heights=[0.2, 0.2, 0.2, 0.2, 0.2],
     # specs=[[{"type": "Scatter3d", "colspan": 4}, None, None, None],
     #        [{"type": "Histogram"}, {"type": "Histogram"}, {"type": "Histogram"}, {"type": "Histogram"}]],
     shared_xaxes=True,
     vertical_spacing=0.02,
     # subplot_titles=[f'All', 'CD68 / Macrophage', 'T-Helper', 'T-Regulatory'],
-    subplot_titles=[f'All', 'CD68 / Macrophage', 'T-Helper', 'T-Killer'],
+    subplot_titles=subplot_titles,
     specs=[[{"secondary_y": False}],
+           [{"secondary_y": True}],
            [{"secondary_y": True}],
            [{"secondary_y": True}],
            [{"secondary_y": True}], ]
@@ -197,52 +120,63 @@ fig = make_subplots(
 #                             box_visible=True, line_color=color_dict[skin_type], meanline_visible=False),
 #                   secondary_y=False, row=1, col=1, )
 
-cell_type_list = ['', 'CD68', 'T-Helper', 'T-Killer']
-cell_type_dict = {'': 'All',
-                  'CD68': 'CD68 / Macrophage',
-                  'T-Helper': 'T-Helper',
-                  'T-Reg': 'T-Regulator',
-                  'T-Killer': 'T-Killer'}
+
 for cell_type in cell_type_list:
     for skin_type in ['Sun-Exposed', 'Non-Sun-Exposed']:
         fig.add_trace(
             go.Violin(x=n_data['Age'][(n_data['Skin Type'] == skin_type) & (n_data['type'].str.contains(cell_type))],
-                      y=n_data['distance'][(n_data['Skin Type'] == skin_type) &
-                                           (n_data['type'].str.contains(cell_type))],
+                      y=n_data['vessel_distance'][(n_data['Skin Type'] == skin_type) &
+                                                  (n_data['type'].str.contains(cell_type))],
                       name=skin_type,
                       points=False, opacity=opacity_dict[skin_type], width=4,
-                      legendgroup=cell_type_dict[cell_type], scalegroup='', scalemode='width',
-                      legendgrouptitle_text=cell_type_dict[cell_type],
+                      legendgroup=cell_dict[cell_type]['legend'], scalegroup='', scalemode='width',
+                      legendgrouptitle_text=cell_dict[cell_type]['legend'],
                       box_visible=True, box_fillcolor='white', line_width=1,
                       line_color=color_dict[f'{cell_type}-{skin_type}'], meanline_visible=False),
             secondary_y=False, row=cell_type_list.index(cell_type) + 1, col=1)
 
-        if cell_type != '':
-            line_ages = [age for age in ages if suns[ages.index(age)] == skin_type]
-            line_ages.sort()
-            fig.add_trace(
-                go.Scatter(x=line_ages,
-                           # y=[percentage_dict[cell_type][ages.index(age)] * 100 for age in line_ages],
-                           y=[len(n_data[(n_data['type'].str.contains(cell_type)) & (n_data['Age'] == age)])
-                              / len(n_data[n_data['Age'] == age])
-                              * 100 for age in line_ages],
-                           mode='lines+markers', marker_symbol='cross',
-                           name=skin_type + " percentage", legendgroup=cell_type_dict[cell_type],
-                           line=dict(color=color_dict[f'{cell_type}-{skin_type}'], width=1), ),
-                secondary_y=True, row=cell_type_list.index(cell_type) + 1, col=1)
+        # if cell_type != '':
+        #     line_ages = [age for age in ages if suns[ages.index(age)] == skin_type]
+        #     line_ages.sort()
+        #     fig.add_trace(
+        #         go.Scatter(x=line_ages,
+        #                    # y=[percentage_dict[cell_type][ages.index(age)] * 100 for age in line_ages],
+        #                    y=[len(n_data[(n_data['type'].str.contains(cell_type)) & (n_data['Age'] == age)])
+        #                       / len(n_data[n_data['Age'] == age])
+        #                       * 100 for age in line_ages],
+        #                    mode='lines+markers', marker_symbol='cross',
+        #                    name=skin_type + " percentage", legendgroup=cell_type_dict[cell_type],
+        #                    line=dict(color=color_dict[f'{cell_type}-{skin_type}'], width=1), ),
+        #         secondary_y=True, row=cell_type_list.index(cell_type) + 1, col=1)
 
 # fig.update_traces(meanline_visible=True,
 #                   scalemode='width')  # scale violin plot area with total count
+main_subtitle = f'<br><sup>Nearest distance from cells to Endothelial cells</sup>'
 fig.update_layout(
-    title="Nearest distance distribution",
+    title=f'Vascular Common Coordinate Framework Violin Chart {main_subtitle}',
+    font=dict(
+        family="Arial, Bahnschrift",
+        size=14,
+        # color="RebeccaPurple"
+    ),
     # x1axis_title="Age",
     # y4axis_title="Nearest Distance",
     violingap=0.3, violingroupgap=0,
     violinmode='overlay',
     yaxis_zeroline=False)
-fig.update_xaxes(title_text="Age", row=4, col=1)
-fig.update_yaxes(title_text="Nearest Distance", row=2, col=1)
+fig.update_xaxes(title_text="Age", row=5, col=1)
+fig.update_yaxes(title_text="Nearest Distance to Endothelial Cells", row=3, col=1)
 fig.update_yaxes(title_text="Percentage", row=2, col=1, secondary_y=True)
+fig.update_yaxes(range=[-99, 550])
+
+# subtitle location
+vertical_offset = 0.02
+for annotation in fig['layout']['annotations']:
+    annotation['xanchor'] = "center"
+    annotation['y'] -= vertical_offset
+    annotation['font'] = dict(
+        family="Arial, Bahnschrift",
+        size=16, )
 
 fig.write_html(os.path.join(target_root_path, f"violin.html"))
 fig.show()

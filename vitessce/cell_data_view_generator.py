fe98de96d49eaea9018e9d8ddfae6efebcfc188b
juyingnan <bunny_gg@hotmail.com>
2023-07-26 00:58:42 -0700

    updated skin damage data

--- vitessce/cell_data_view_generator.py
+++ vitessce/cell_data_view_generator.py
@@ -1,10 +1,11 @@
 import pandas as pd
 
 cell_data = {
-    'cell_id': [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    'cell_id': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
     'cell_type': ['T-Killer', 'T-Helper', 'T-Reg', 'CD68', 'vessel',
-                  "T-Killer_link", 'T-Helper_link', 'T-Reg_link', 'CD68_link']
+                  "T-Killer_link", 'T-Helper_link', 'T-Reg_link', 'CD68_link',
+                  'DDB2', 'P53', 'KI67', 'skin'],
 }
 cell_sets_data = pd.DataFrame(cell_data)
 
-cell_sets_data.to_csv('vccf_cell_sets.csv', index=False)
+cell_sets_data.to_csv('cell_sets.csv', index=False)
